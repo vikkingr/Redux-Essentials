3fcb65c6f7242b823f3253f292ddaadf2746221e
Robert Veinberg-Vikking <robivikking98@hotmail.com>
2022-06-24 13:51:42 -0400

    singlePostPage: add link

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 export const SinglePostPage = ({ match }) => {
     const { postId } = match.params
@@ -31,6 +32,11 @@ export const SinglePostPage = ({ match }) => {
                 >
                     {post.content}
                 </p>
+                <Link
+                    
+                >
+
+                </Link>
             </article>
         </section>
     )
