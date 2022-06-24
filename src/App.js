03f27a52f2817e357c006c8cbf360e50ccb134b5
Robert Veinberg-Vikking <robivikking98@hotmail.com>
2022-06-23 20:33:34 -0400

    edit post form, route edit post form

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 
 import { Navbar } from './app/Navbar'
 import { AddPostForm } from './features/posts/AddPostForm'
+import { EditPostForm } from './features/posts/EditPostForm'
 import { PostsList } from './features/posts/PostsList'
 import { SinglePostPage } from './features/posts/SinglePostPage'
 
@@ -27,7 +28,16 @@ function App() {
               </React.Fragment>
             )}
           />
-          <Route exact path="/posts/:postId" component={SinglePostPage} />
+          <Route 
+            exact
+            path="/posts/:postId"
+            component={SinglePostPage} 
+          />
+          <Route 
+            exact
+            path="/editPost/:postId"
+            component={EditPostForm}
+          />
           <Redirect to="/" />
         </Switch>
       </div>
